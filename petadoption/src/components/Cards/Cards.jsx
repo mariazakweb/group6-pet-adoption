c418c6b94f23de555a3ea4452bd1e123d8f2ba6b
Amanda Fuhrer <amandafuhrer@gmail.com>
2023-06-24 13:41:58 -0400

    created a card component

---
+++ petadoption/src/components/Cards/Cards.jsx
@@ -0,0 +1,14 @@
+// pet profile cards
+
+function Cards() {
+  return (
+    <>
+      <a href="">
+        <article class="cards__card">
+          <h3 class="cards__title"></h3>
+          <img class="cards__image" src="" alt=""></img>
+        </article>
+      </a>
+    </>
+  );
+}
