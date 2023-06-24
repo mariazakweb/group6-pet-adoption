32833788ca5e366e062fcdad267329bd0806f3d9
Melissa <66793695+MissmelissaL@users.noreply.github.com>
2023-06-24 14:16:14 -0400

    massive commit

--- petadoption/src/App.js
+++ petadoption/src/App.js
@@ -14,7 +14,6 @@ function App() {
   return (
     <body>
       <BrowserRouter>
-        <NavComp/>
         <Routes>
           <Route path="/" element={<HomePage/>} />
           <Route path="/pet/:id" element={<PetProfilePage />}/>
--- petadoption/src/pages/PetProfilePage.jsx
+++ petadoption/src/pages/PetProfilePage.jsx
@@ -1,9 +1,13 @@
 // PetProfilePage
+import { useState, useEffect, useRef} from "react";
+import axios from "axios";
 
 function PetProfilePage() {
+    const [selectedPet, setSelectedPet] = useState([]);
 
+    
     return (
-        <h1>Pet Profile Page</h1>
+        <h1>{"Pet Profile Page" && selectedPet.animal.name}</h1>
     )
 }
 
